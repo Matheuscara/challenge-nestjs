75c83cc697922ed69f4dd3d8e7ab06759d2f5c39
matheus dias cara <matheusdiascara@192.168.0.100>
2025-06-26 00:02:22 -0300

    Feat: adicionando configuracao para o helmet :)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { getHelmetConfig } from './config/helmet.config';
 import { TransformInterceptor } from './utils/interceptors/transform.interceptor';
 import { DomainErrorInterceptor } from './utils/interceptors/domain-error.interceptor';
 
@@ -9,6 +11,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
   app.useLogger(app.get(Logger));
+
+  app.use(helmet(getHelmetConfig()));
 
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalInterceptors(new DomainErrorInterceptor());
